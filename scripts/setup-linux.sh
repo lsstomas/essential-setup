7b38d264002345c97557bb523e7a1182c53aaea0
Luis <luisftomasprado@gmail.com>
2024-11-19 03:06:47 -0300

    chore: add initial project structure and README

---
+++ scripts/setup-linux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script para configuração inicial do Linux
+# Instruções: Execute com "bash setup-linux.sh"
+
+# Atualização do sistema
+sudo apt update && sudo apt upgrade -y
+
+# Instalar pacotes essenciais
+sudo apt install -y git curl wget zsh vim
